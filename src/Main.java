cdda869f9f3d3220bf0af4d8f70b32a1462de548
Ron Seubers <ronseubers@gmail.com>
2023-05-15 14:01:05 +0200

    nieuwe structuur

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        IntroductieClass tijdelijk = new IntroductieClass("Ron");
-        IntroductieClass tijdelijk2 = new IntroductieClass("Henk");
+        IntroductieClass tijdelijk = new IntroductieClass("Ron", 33);
+        IntroductieClass tijdelijk2 = new IntroductieClass("Henk", 35);
     }
 }
