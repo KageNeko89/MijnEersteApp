b53f046d0ef3af3eee50faf6b98dda9ea40b132e
Ron Seubers <ronseubers@gmail.com>
2023-05-15 14:14:06 +0200

    nieuwe  structuur

---
+++ src/IntroductieClass.java
@@ -0,0 +1,9 @@
+public class IntroductieClass {
+    public String name;
+    public Integer age;
+    public IntroductieClass(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("hoi " + name + ", " + age + " jaar oud.");
+    }
+}
